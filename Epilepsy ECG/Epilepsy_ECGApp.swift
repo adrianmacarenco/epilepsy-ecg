a4090e65079583fe2ef10bbc42235ef642f1341f
Adrian Macarenco <adrian.macarenco@gmail.com>
2023-03-05 15:37:59 +0100

    Initial Commit

---
+++ Epilepsy ECG/Epilepsy_ECGApp.swift
@@ -0,0 +1,17 @@
+//
+//  Epilepsy_ECGApp.swift
+//  Epilepsy ECG
+//
+//  Created by Adrian Macarenco on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Epilepsy_ECGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
