4a7808f373147af7a0fd2d93894bf08eb5414c7d
Adrian Macarenco <adrian.macarenco@gmail.com>
2023-03-18 14:28:35 +0100

    feat: bluetooth client added

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "BluetoothClient",
+            targets: ["BluetoothClient"]),
+        .library(
             name: "Packages",
             targets: ["Packages"]),
     ],
@@ -18,6 +21,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "BluetoothClient",
+            dependencies: []
+        ),
         .target(
             name: "Packages",
             dependencies: []),
---
+++ Packages/Sources/BluetoothClient/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adrian Macarenco on 05/03/2023.
+//
+
+import Foundation
