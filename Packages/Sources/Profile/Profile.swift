44154b729dc41b9711d959f7e65c6c1233e989c8
adrianmacarenco <adrian.macarenco@gmail.com>
2023-05-03 16:01:10 +0300

    feat: add device Model

---
+++ Packages/Sources/Profile/Profile.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adrian Macarenco on 01/05/2023.
+//
+
+import Foundation
+import SwiftUI
+import StylePackage
+
+public struct ProfileView: View {
+    public init() {
+        
+    }
+    
+    public var body: some View {
+        VStack {
+            Text("Profile view")
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+        .background(Color.background)
+    }
+}
+
