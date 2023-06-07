1807c049cd64390bff9ed35b236068ff6425ac1a
adrianmacarenco <adrian.macarenco@gmail.com>
2023-06-07 19:45:47 +0200

    feat: present confirmation popup when skipping profile setup

--- Packages/Sources/UserCreation/GettingStarted.swift
+++ Packages/Sources/UserCreation/GettingStarted.swift
@@ -19,7 +19,13 @@ import XCTestDynamicOverlay
 public class GettingStartedViewModel: ObservableObject {
     enum Destination {
         case personalIdentity(PersonalIdentityViewModel)
+        case alert(AlertState<AlertAction>)
     }
+    
+    public enum AlertAction {
+      case confirmSetUpLater
+    }
+    
     @Published var route: Destination?
     @Dependency (\.dbClient) var dbClient
     @Dependency (\.persistenceClient) var persistenceClient
@@ -51,11 +57,17 @@ public class GettingStartedViewModel: ObservableObject {
     }
     
     func skipButtonTapped() {
-        Task {
-            let savedUser = try await dbClient.createUser(localUser.id, localUser.fullName, localUser.birthday, localUser.gender, localUser.weight, localUser.height, localUser.diagnosis)
-            persistenceClient.user.save(savedUser)
-            userCreationFlowEnded()
-        }
+        route = .alert(.setUpLater)
+    }
+    func alertButtonTapped(_ action: AlertAction) {
+      switch action {
+      case .confirmSetUpLater:
+          Task {
+              let savedUser = try await dbClient.createUser(localUser.id, localUser.fullName, localUser.birthday, localUser.gender, localUser.weight, localUser.height, localUser.diagnosis)
+              persistenceClient.user.save(savedUser)
+              userCreationFlowEnded()
+          }
+      }
     }
 }
 
@@ -87,6 +99,13 @@ public struct GettingStartedView: View {
             .padding(.horizontal, 16)
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
             .background(Color.background)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Set up later") {
+                        vm.skipButtonTapped()
+                    }
+                }
+            }
             .navigationDestination(
                 unwrapping: self.$vm.route,
                 case: /GettingStartedViewModel.Destination.personalIdentity
@@ -95,22 +114,24 @@ public struct GettingStartedView: View {
                     .navigationBarTitleDisplayMode(.inline)
 
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Set up later") {
-                        vm.skipButtonTapped()
-                    }
-                }
+            .alert(
+              unwrapping: self.$vm.route,
+              case: /GettingStartedViewModel.Destination.alert
+            ) { action in
+              self.vm.alertButtonTapped(action)
             }
-            
         }
         .tint(.tint1)
     }
 }
 
-//struct GettingStartedView_Preview: PreviewProvider {
-//    static var previews: some View {
-//        registerFonts()
-//        return GettingStartedView()
-//    }
-//}
+extension AlertState where Action == GettingStartedViewModel.AlertAction {
+  static let setUpLater = AlertState(
+    title: TextState("Set up later"),
+    message: TextState("Are you sure you want to skip the initial setup process? Remember, you can always modify your information in the profile section at a later time."),
+    buttons: [
+        .destructive(TextState("Yes"), action: .send(.confirmSetUpLater)),
+      .cancel(TextState("No"))
+    ]
+  )
+}
