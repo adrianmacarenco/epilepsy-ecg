f4711ea247277ada992af4cd4b97d0a8a6e3cffc
adrianmacarenco <adrian.macarenco@gmail.com>
2023-06-19 12:42:41 +0200

    feat: observable localisations

---
+++ Packages/Sources/Localizations/DependencyKey.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adrian Macarenco on 15/06/2023.
+//
+
+import Foundation
+import Dependencies
+
+extension ObservableLocalizations: TestDependencyKey {
+    public static var testValue: ObservableLocalizations {
+        .bundled
+    }
+}
+
+public extension DependencyValues {
+    var localizations: ObservableLocalizations {
+        get { self[ObservableLocalizations.self] }
+        set { self[ObservableLocalizations.self] = newValue }
+    }
+}
